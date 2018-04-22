6329a32e9ceb68bab3ce83ec3a45ddb1ec90ff39
Benjamin Cable <cable.benjamin@gmail.com>
2018-04-22 12:33:18 +0100

    bunch of fixes

---
+++ newN4t/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  newN4t
+//
+//  Created by Benjamin Cable on 15/04/2018.
+//  Copyright © 2018 Benjamin Cable. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
